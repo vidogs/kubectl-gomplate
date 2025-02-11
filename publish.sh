16bc073a59986001fc8bb0fa5d2e108fc505ebc3
Denis <admin@doubleds.ru>
2025-02-11 13:53:21 +0100

    Build with arch linux/amd64

--- publish.sh
+++ publish.sh
@@ -2,11 +2,11 @@
 
 set -xe
 
-docker build -t vidog/kubectl-gomplate:0.1 .
+docker buildx build --platform linux/amd64 -t vidog/kubectl-gomplate:0.2 .
 
 docker push vidog/kubectl-gomplate:0.1
 
-docker build -t vidog/kubectl-gomplate:latest .
+docker buildx build --platform linux/amd64 -t vidog/kubectl-gomplate:latest .
 
 docker push vidog/kubectl-gomplate:latest
 
